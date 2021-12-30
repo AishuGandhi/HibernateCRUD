fcf99561bb590d83241bd9f8ddaaa54a62d70360
Aishwarya <aishwarya.gandhi@finastra.com>
2021-12-30 20:08:17 +0530

    Java Multithread Example

--- CoreJava/src/multithreading/MultiThread.java
+++ CoreJava/src/multithreading/MultiThread.java
@@ -1,11 +1,57 @@
 package multithreading;
 
+import com.sun.istack.internal.logging.Logger;
+
 public class MultiThread
 {
 
     public static void main(String[] args)
     {
+        A a = new A();
+        a.start();
+        
+        B b = new B();
+        b.start();
 
     }
 
+}
+
+class A extends Thread
+{
+    void show()
+    {
+        for (int i = 0; i <= 100; i++)
+        {
+            System.out.println("Class A Hiii");
+            try
+            {
+                Thread.sleep(1000);
+            }
+            catch(InterruptedException ie)
+            {
+              //Logger.getLogger(A.class.getName()).log();   
+            }
+        }
+    }
+    public void run()
+    {
+        show();
+    }
+
+}
+
+class B extends Thread
+{
+    void show()
+    {
+        for (int i = 0; i <= 100; i++)
+        {
+            System.out.println("Class B Hiii");
+        }
+    }
+    public void run()
+    {
+        show();
+    }
 }
