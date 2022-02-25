8ff8891d492d16db2df4463a587cc14f8ba472e7
Aishwarya <aishwarya.gandhi@finastra.com>
2022-02-25 19:39:10 +0530

    Stream and Lambda Expressions

--- CoreJava/src/abstractioninterface/InterfaceDemo.java
+++ CoreJava/src/abstractioninterface/InterfaceDemo.java
@@ -4,13 +4,47 @@ interface A
 {
   void show();    
 }
-class B implements A
+interface C
+{
+    
+  void show();    
+  static void display()
+  {
+      System.out.println("Static method in Interface");
+  }
+}
+interface I
+{
+    
+ default void show()
+ {
+     System.out.println("Default from Show I interface");
+ }
+}
+class D
+{
+  public void show()
+  {
+      System.out.println("In Show from class D");
+  }
+}
+class B extends D implements I
 {
   public void show()
   {
       System.out.println("Hellloo Interface Demo");
   }
 }
+class E extends D implements I
+{
+    
+    
+}
+/*
+   Marker Interface -> Interface Without Any Methods
+   SAM/Functional Interface -> Single Abstract Method having only 1 method For example: Runnable
+   Normal Interface
+ */
 public class InterfaceDemo
 {
 
@@ -18,7 +52,20 @@ public class InterfaceDemo
     {
       B b = new B();
       b.show();
+      E e = new E();
+      e.show();
+      C.display();
+      /*A a = new A()
+      {
+          public void show()
+          {
+              System.out.println("In Show");
+          }
+      };
+      a.show();*/
+      //Lambda Expression
+      A a = () ->System.out.println("In Show");
+      a.show();
     }
-
 }
 
